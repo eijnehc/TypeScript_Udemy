8dfc955e57ecf9efc5ec2f361151d8d0731ef9ae
Chen Jie <e0036922@u.nus.edu>
2021-03-16 02:39:23 +0800

    Finish 1 quarter generics

---
+++ generics-01-starting-setup/src/app.ts
@@ -0,0 +1,19 @@
+// const names: Array<string> = ['Max', 'Manuel']; // string[]
+// // names[0].split(' ');
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done');
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split(' ');
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.age);
